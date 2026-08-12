3e314abfe537be744dc357fa7124257ce7e72eaf
anshulbhatt1614-a11y <anshulbhatt1614@gmail.com>
2026-08-12 10:56:52 +0530

    Create program1_day1.C

---
+++ program1_day1.C
@@ -0,0 +1,28 @@
+/*
+Input 1:
+3 4
+Output 1:
+Sum = 7
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+*/
+
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("Sum = %d",a+b);
+    return 0;
+}
+/*PS C:\Users\anshu\Anshul> cd "c:\Users\anshu\Anshul\" ; if ($?) { gcc program1_day1.C -o program1_day1 } ; if ($?) { .\program1_day1 }
+3
+4
+Sum = 7
+PS C:\Users\anshu\Anshul> cd "c:\Users\anshu\Anshul\" ; if ($?) { gcc program1_day1.C -o program1_day1 } ; if ($?) { .\program1_day1 }
+-1
+20
+Sum = 19
+PS C:\Users\anshu\Anshul> */
