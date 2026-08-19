bb71e13b6d3e9a53711d3d81cd4f8904adc39812
anshulbhatt1614-a11y <anshulbhatt1614@gmail.com>
2026-08-19 09:02:58 +0530

    Create program1_day10.C

---
+++ program1_day10.C
@@ -0,0 +1,54 @@
+/*Q19: Write a program to classify a triangle as Equilateral, Isosceles, or Scalene based on its side lengths.
+
+Sample Test Cases:
+Input 1:
+3 3 3
+Output 1:
+Equilateral
+
+Input 2:
+3 3 4
+Output 2:
+Isosceles
+
+Input 3:
+2 3 4
+Output 3:
+Scalene
+
+*/
+#include <stdio.h>
+int main()
+{
+ int a,b,c;
+ scanf("%d%d%d",&a,&b,&c);
+ if(a==b && b==c && c==a)
+ {
+    printf("Equilateral");
+ }   
+ else if(a==b || a==c || b==c )
+ {
+    printf("Isosceles");
+ }
+ else 
+ {
+    printf("Scalene");
+ }
+ return 0;
+}
+/*PS C:\Users\anshu\OneDrive> cd "c:\Users\anshu\OneDrive\" ; if ($?) { gcc tempCodeRunnerFile.c -o tempCodeRunnerFile } ; if ($?) { .\tempCodeRunnerFile }
+3
+3
+3
+Equilateral
+PS C:\Users\anshu\OneDrive> cd "c:\Users\anshu\OneDrive\" ; if ($?) { gcc tempCodeRunnerFile.c -o tempCodeRunnerFile } ; if ($?) { .\tempCodeRunnerFile }
+3
+3
+4
+Isosceles
+PS C:\Users\anshu\OneDrive> cd "c:\Users\anshu\OneDrive\" ; if ($?) { gcc tempCodeRunnerFile.c -o tempCodeRunnerFile } ; if ($?) { .\tempCodeRunnerFile }
+2
+3
+4
+Scalene
+PS C:\Users\anshu\OneDrive> */
