13567631307b39746de5098091d89a75d04408c3
anshulbhatt1614-a11y <anshulbhatt1614@gmail.com>
2026-08-31 11:03:54 +0530

    Add files via upload

---
+++ program1_day18.c
@@ -0,0 +1,36 @@
+/*Q35: Write a program to print all factors of a given number.
+
+Sample Test Cases:
+Input 1:
+6
+Output 1:
+1 2 3 6
+
+Input 2:
+10
+Output 2:
+1 2 5 10
+
+*/
+#include <stdio.h>
+int main()
+{
+int i,n;
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+printf("%d ",i);
+}
+}
+return  0;
+}
+/*
+C:\cproject>program1_day18.out
+6
+1 2 3 6
+C:\cproject>program1_day18.out
+10
+1 2 5 10
+C:\cproject>*/
