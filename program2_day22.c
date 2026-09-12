40bbdaad2438fd67f7c0ba35f9775b1ae5c444af
anshulbhatt1614-a11y <anshulbhatt1614@gmail.com>
2026-09-12 23:21:27 +0530

    Add files via upload

---
+++ program2_day22.c
@@ -0,0 +1,35 @@
+/*Q44: Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+Approximate sum: 3.3
+
+Input 2:
+5
+Output 2:
+Approximate sum: 4.4
+
+*/
+#include <stdio.h>
+int main()
+{
+int a=3,b=4,n,i;
+float s=0.0f;
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+ {
+ s=s+((float)a/b);
+ }
+ printf("Approximate sum: %.1f",1+s);
+ return 0;
+ }
+ /*C:\cproject>program2_day22.out
+3
+Approximate sum: 3.2
+C:\cproject>program2_day22.out
+5
+Approximate sum: 4.8
+C:\cproject>*/
+ 
